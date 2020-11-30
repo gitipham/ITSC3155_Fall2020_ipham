34a7bc3cac6a33ef6d4493aeb1baf27f54cf0722
Ivy Pham <ipham@uncc.edu>
2020-11-30 09:27:27 -0500

    flask app #4 - added db connection to app

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -5,8 +5,16 @@ from flask import Flask                # Flask is the web app that we will custo
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
+from database import db
+
 
 app = Flask(__name__)     # create an app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '11-1-2020'},
         2: {'title': 'Second note', 'text': 'This is my second note', 'date': '11-2-2020'},
