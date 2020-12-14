1c647d5cee5f9464ff02ce273cfaeddedaa44bde
Ivy Pham <ipham@uncc.edu>
2020-12-14 00:36:53 -0500

    flask app #5 - added delete functionality

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -64,6 +64,14 @@ def update_note(note_id):
         my_note = db.session.query(Note).filter_by(id = note_id).one()
         return render_template('new.html', note = my_note, user = a_user)
 
+@app.route('/notes/delete/<note_id>', methods=['POST'])
+def delete_note(note_id):
+    # retrieve note from database
+    my_note = db.session.query(Note).filter_by(id = note_id).one()
+    db.session.delete(my_note)
+    db.session.commit()
+    return redirect(url_for('get_notes'))
+
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
     if request.method == 'POST':
